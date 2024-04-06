bdf483fb180f836bd5a6d326807e8fb9e83b7e92
igpraise <igpraise45@gmail.com>
2024-04-05 20:51:21 -0400

    Function to process a flight data file

--- major 4/major 4.cpp
+++ major 4/major 4.cpp
@@ -30,4 +30,28 @@ int parseLine(char* source, char* destination, int* fare, char* line) {
     *fare = atoi(strtok(NULL, "-"));
     return 1;
 }
-
+int processFlight(char* filename, struct Flight flights[], int* total_count) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error: Could not open file %s\n", filename);
+        return 0;
+    }
+    char line[MAX_LINE_LENGTH];
+    while (fgets(line, sizeof(line), file) != NULL && *total_count < MAX_FLIGHTS) {
+        if (line[0] == '\n') continue;
+        char source[20], destination[20];
+        int fare;
+        if (!parseLine(source, destination, &fare, line)) {
+            printf("Error: Invalid format in file %s\n", filename);
+            fclose(file);
+            return 0;
+        }
+        strcpy(flights[*total_count].source, source);
+        strcpy(flights[*total_count].destination, destination);
+        flights[*total_count].fare = fare;
+        strcpy(flights[*total_count].airline, strtok(filename, "."));
+        (*total_count)++;
+    }
+    fclose(file);
+    return 1;
+}
